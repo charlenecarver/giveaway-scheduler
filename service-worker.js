1bf72d706d2c95a8cac11c45fbc8db2cb69c6eac
Charlene Carver <charlenejoaquin@gmail.com>
2026-08-28 10:44:45 -0400

    Fix giveaway replacement and refresh live profiles

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "givvy-time-v66";
+const CACHE_NAME = "givvy-time-v67";
 const IMAGE_CACHE_NAME = "givvy-time-images-v3";
 const REMOTE_IMAGE_TIMEOUT_MS = 8000;
 const APP_SHELL = [
