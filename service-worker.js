8bade6ae11b756b04e2b5e4f65c37e59bb0dedcf
Charlene Carver <charlenejoaquin@gmail.com>
2026-08-28 14:27:07 -0400

    Save edited giveaway items to selected lives

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "givvy-time-v70";
+const CACHE_NAME = "givvy-time-v71";
 const IMAGE_CACHE_NAME = "givvy-time-images-v3";
 const REMOTE_IMAGE_TIMEOUT_MS = 8000;
 const APP_SHELL = [
