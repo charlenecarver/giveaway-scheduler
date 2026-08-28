a2d24c0988d4019171ddfb8d31bd1bd3f0fd7dc9
Charlene Carver <charlenejoaquin@gmail.com>
2026-08-28 15:36:24 -0400

    Prompt before marking countdown missed

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "givvy-time-v74";
+const CACHE_NAME = "givvy-time-v75";
 const IMAGE_CACHE_NAME = "givvy-time-images-v3";
 const REMOTE_IMAGE_TIMEOUT_MS = 8000;
 const APP_SHELL = [
