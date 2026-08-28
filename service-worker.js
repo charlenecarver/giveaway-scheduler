8a74e5aa6975fde8c01fea212dfc8ea506b2ee49
Charlene Carver <charlenejoaquin@gmail.com>
2026-08-28 15:16:13 -0400

    Improve mobile live management

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "givvy-time-v71";
+const CACHE_NAME = "givvy-time-v74";
 const IMAGE_CACHE_NAME = "givvy-time-images-v3";
 const REMOTE_IMAGE_TIMEOUT_MS = 8000;
 const APP_SHELL = [
