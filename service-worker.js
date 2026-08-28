ea9f70e372118bcc26071c89c34220b97b827d2b
Charlene Carver <charlenejoaquin@gmail.com>
2026-08-28 02:38:54 -0400

    Remove remaining live status toggle

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "givvy-time-v53";
+const CACHE_NAME = "givvy-time-v54";
 const IMAGE_CACHE_NAME = "givvy-time-images-v3";
 const REMOTE_IMAGE_TIMEOUT_MS = 8000;
 const APP_SHELL = [
